a5638760e3dbc2893291c54932034a1a2988173a
tmrln2709 <tmrln2709@gmail.com>
2023-03-07 23:14:42 +0600

    initial commit

---
+++ script.js
@@ -0,0 +1,78 @@
+// let a = 'hello world'
+// let b = 'great day'
+// let c = b.slice(0,b.indexOf(' day'))
+// let d = a.slice(a.indexOf(' '))
+// console.log(c + d)
+
+// let a = 'hello world'
+//
+// console.log(a.replaceAll('l', 'k'))
+
+// let a = 'lorem ipsum dolor'
+// let b = a.slice(0, 15)
+// let c = a.slice(15, 17).replace('o', 'a')
+//
+// console.log(b + c)
+
+// let a = 'hello'
+// let b = 'Ivan'
+// let c = (a.charAt(0).toUpperCase() + a.slice(1) + ' ' + b.toLowerCase())
+// console.log(c)
+
+// let a = 'lorem ipsum dolor'
+//
+// console.log(a.length)
+
+// let a = 'lorem ipsum dolor sit amet'
+// let b = a.slice(18, 22)
+// let c = a.slice(a.indexOf(''), 18) + a.slice(22, 27)
+//
+// console.log(b + c)
+
+// let a = 'children'
+// let b = a.slice(a.indexOf(''), 7).toUpperCase() + a.slice(7)
+// console.log(b)
+
+
+// let a = 'boss'
+// let b = a.slice(a.indexOf(''), 3)
+// let c = b.replace('o', 's') +a.slice(1, 2)
+// console.log(c)
+
+
+// let a = 'sunday'
+//
+// console.log(a.indexOf('d'))
+
+// let a = 'hello'
+// let b = a.replace('l', 'k')
+//
+// console.log(b.replace(('l'), 's'))
+
+// let a = 'user'
+// let b = 'new'
+//
+// console.log(b + ' ' + a.slice(0, 2))
+
+// let a = 'monday'
+// let b = 'day'
+//
+// console.log(b + a.slice(3, 6) + a.slice(0, 3))
+
+// let a = 'HELLO'
+// let b = a.slice(0, 1).toLowerCase() + a.slice(1, 2) + a.slice(2, 3).toLowerCase() + a.slice(3, 4) + a.slice(4, 5).toLowerCase()
+//
+// console.log(b)
+
+// let a = 5
+// let b = '400'
+// let c = a.toString()
+// let d = b.replace('0', a)
+//
+// console.log(d)
+
+// let a = 'monkey'
+// let b = 'banana'
+// let c = a.slice(0, 1) + b.slice(1, 4) + a.slice(3, 4)
+//
+// console.log(c)
